ec17f1f09f7d75734199f77db3ae8d295ddcea8e
celinacarnevale <102771516+celinacarnevale@users.noreply.github.com>
2023-05-09 20:43:24 -0300

    Commit 09/05 added create persona function

--- src/main/java/com/portfolio/celinavillarroel/Controller/PersonaController.java
+++ src/main/java/com/portfolio/celinavillarroel/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +38,19 @@ public class PersonaController {
             return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
         Persona persona = sPersona.getOne(id).get();
         return new ResponseEntity(persona, HttpStatus.OK);
+    }
+    
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoPersona dtopers){
+        if(StringUtils.isBlank(dtopers.getNombre()))
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio, no puede estar en blanco"), HttpStatus.BAD_REQUEST);
+        
+        if(sPersona.existsByNombre(dtopers.getNombre()))
+            return new ResponseEntity(new Mensaje("Esta persona ya existe."), HttpStatus.BAD_REQUEST);
+        
+        Persona persona = new Persona(dtopers.getNombre(),dtopers.getApellido(), dtopers.getDescripcion());
+        sPersona.save(persona);
+        return new ResponseEntity(new Mensaje("Persona agregada con exito."), HttpStatus.OK);
     }
     
     @PutMapping("/update/{id}")
